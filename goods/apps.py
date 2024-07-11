9fc15c355e3bd96ec57480e8f0d1cb330a1b2873
BronkstoneBro <atlantwindow@gmail.com>
2024-07-11 15:26:05 +0300

    Created Category, Goods, Product, all settings, migrations

--- goods/apps.py
+++ goods/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
-class MainConfig(AppConfig):
+class GoodsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "main"
+    name = "goods"
+    verbose_name = "Товары"
