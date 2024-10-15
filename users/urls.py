a79557eca9fb2d83738577f3bbdfeb9bca257fcf
BronkstoneBro <atlantwindow@gmail.com>
2024-10-15 14:54:59 +0300

    all cart templates and includes

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('registration', views.registration, name='registration'),
     path('logout', views.logout, name='logout'),
     path('profile', views.profile, name='profile'),
+    path('users-cart', views.users_cart, name='users_cart'),
 
 ]
