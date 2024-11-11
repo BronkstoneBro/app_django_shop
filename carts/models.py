36a4d3a1cf317a7a2abab1d838268da0efdc5671
BronkstoneBro <atlantwindow@gmail.com>
2024-11-11 15:46:30 +0200

    Create shopping cart

--- carts/models.py
+++ carts/models.py
@@ -9,12 +9,14 @@ class CartQuerySet(models.QuerySet):
         return [cart.product_price() for cart in self]
 
     def total_quantity(self):
-        return sum(cart.quantity for cart in self)
+        return sum(cart.quantity for cart in self) if self else 0
+
 
 class Cart(models.Model):
-    user = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name="Пользователь")
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE,blank=True, null=True, verbose_name="Пользователь")
     product = models.ForeignKey(to=Products, on_delete=models.CASCADE, verbose_name="Продукт")
     quantity = models.PositiveSmallIntegerField(default=0, verbose_name="Количество")
+    session_key = models.CharField(max_length=40, blank=True, null=True, verbose_name="Ключ сессии")
     created_timestamp = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
 
     class Meta:
@@ -22,6 +24,7 @@ class Cart(models.Model):
         verbose_name_plural = "Корзины"
 
     objects = CartQuerySet().as_manager()
+
     def product_price(self):
         return round(self.product.sell_price() * self.quantity, 2)
 
